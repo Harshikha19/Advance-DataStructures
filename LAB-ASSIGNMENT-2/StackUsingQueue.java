248fd00d2c3d0dd567954a2cbb0b7b5ce88bd9e3
Harshikha19 <59283579+Harshikha19@users.noreply.github.com>
2023-09-01 01:18:40 +0530

    Create StackUsingQueue.java

---
+++ LAB-ASSIGNMENT-2/StackUsingQueue.java
@@ -0,0 +1,45 @@
+import java.util.Queue;
+
+public class StackUsingQueue {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+
+    public StackUsingQueue() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    // Push an element into the stack.
+    public void push(int data) {
+        // Move all elements from q1 to q2.
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+
+        // Push the given item into q1.
+        q1.add(data);
+    }
+
+    // Pop an element from the stack.
+    public int pop() {
+        // Check if the stack is empty.
+        if (q1.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+
+        // Move all elements from q1 to q2 except the last element.
+        int data = q1.remove();
+        while (q1.size() > 1) {
+            q2.add(q1.remove());
+        }
+
+        // The last element in q1 is the top element of the stack.
+        return data;
+    }
+
+    // Check if the stack is empty.
+    public boolean isEmpty() {
+        return q1.isEmpty();
+    }
+}
